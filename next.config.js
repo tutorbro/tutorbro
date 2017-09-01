cc586105599c17117ad1628803b8675149918dd8
Vinay Puppal <me@vinaypuppal.com>
2017-09-01 19:05:12 +0530

    fix typo and use preact on prod

--- next.config.js
+++ next.config.js
@@ -1,52 +1,70 @@
-const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const { ANALYZE } = process.env;
 
 module.exports = {
   webpack: (config, { dev }) => {
     /* Enable only in Production */
     if (!dev) {
-      if (config.resolve.alias) {
-        delete config.resolve.alias['react']
-        delete config.resolve.alias['react-dom']
+      // preact
+      console.log('> Using Preact instead of React');
+      config.resolve.alias = {
+        react: 'preact-compat/dist/preact-compat',
+        'react-dom': 'preact-compat/dist/preact-compat',
+      };
+
+      // Analyzer
+      if (ANALYZE) {
+        config.plugins.push(
+          new BundleAnalyzerPlugin({
+            analyzerMode: 'server',
+            analyzerPort: 8888,
+            openAnalyzer: true,
+          }),
+        );
       }
+
       // Service Worker
-      config.plugins.push(new SWPrecacheWebpackPlugin({
-        filename: 'sw.js',
-        minify: true,
-        staticFileGlobsIgnorePatterns: [ /\.next\// ],
-        staticFileGlobs: [
-          // Precache all static files by default
-          '/static/**/*'
-        ],
-        forceDelete: true,
-        runtimeCaching: [
-          {
-            handler: 'fastest',
-            urlPattern: /[.](png|jpg|svg)/
-          },
-          {
-            handler: 'cacheFirst',
-            urlPattern: /\/_next\/.*/,
-            options: {
-              cache: {
-                name: 'nextjs-cache'
-              }
-            }
-          },
-          {
-            handler: 'networkFirst',
-            urlPattern: /^http.*/ // cache all files
-          }
-        ]
-      }))
+      config.plugins.push(
+        new SWPrecacheWebpackPlugin({
+          filename: 'sw.js',
+          minify: true,
+          staticFileGlobsIgnorePatterns: [/\.next\//],
+          staticFileGlobs: [
+            // Precache all static files by default
+            '/static/**/*',
+          ],
+          forceDelete: true,
+          runtimeCaching: [
+            {
+              handler: 'fastest',
+              urlPattern: /[.](png|jpg|svg)/,
+            },
+            {
+              handler: 'cacheFirst',
+              urlPattern: /\/_next\/.*/,
+              options: {
+                cache: {
+                  name: 'nextjs-cache',
+                },
+              },
+            },
+            {
+              handler: 'networkFirst',
+              urlPattern: /^http.*/, // cache all files
+            },
+          ],
+        }),
+      );
     }
 
-    return config
+    return config;
   },
-  exportPathMap: function () {
+  exportPathMap: function() {
     return {
       '/': { page: '/' },
       '/careers': { page: '/careers' },
-      '/chat': { page: '/chat' }
-    }
-  }
-}
+      '/chat': { page: '/chat' },
+    };
+  },
+};
--- pages/careers.js
+++ pages/careers.js
@@ -1,33 +1,34 @@
-import React from 'react'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import { initGA, logPageView } from '../utils/analytics'
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { initGA, logPageView } from '../utils/analytics';
 
 export default class chat extends React.Component {
-  componentDidMount () {
-    initGA()
-    logPageView()
+  componentDidMount() {
+    initGA();
+    logPageView();
   }
-  render () {
+  render() {
     return (
       <div>
-        <Header title='TutorBro | Careers' />
+        <Header title="TutorBro | Careers" />
         <main>
-          <div className='carrers__container'>
-            <section className='carrers'>
+          <div className="carrers__container">
+            <section className="carrers">
               <h1>Job Description</h1>
-              <div className='profile'>
+              <div className="profile">
                 <h4>Profile:</h4>
                 <p>
                   <strong>Subject Matter Expert </strong>
-                  – Mechanical / Electrical / Civil / Matlab / Mathematics / Chemistry / Physics / Accounts / Economics / Finance
+                  – Mechanical / Electrical / Civil / Matlab / Mathematics / Chemistry / Physics /
+                  Accounts / Economics / Finance
                 </p>
               </div>
-              <div className='roles'>
+              <div className="roles">
                 <h4>Roles & Responsibility:</h4>
                 <ul>
                   <li>
-                    Answering quarries & providing solutions on liveQuestion and Answer Portal.
+                    Answering queries & providing solutions on liveQuestion and Answer Portal.
                   </li>
                   <li>Quality Review of solutions from pear tutors/SME. </li>
                   <li>Making step by step solution of US Textbooks. </li>
@@ -35,25 +36,26 @@ export default class chat extends React.Component {
                   <li>Live/online tutoring if required.</li>
                 </ul>
               </div>
-              <div className='candidate'>
+              <div className="candidate">
                 <h4>Desired Candidate:</h4>
                 <ul>
                   <li>
-                    Having bachelors or masters in Mechanical / Electrical / Civil / Matlab / Mathematics / Chemistry / Physics / Accounts / Economics / Finance with grade more than 60%.
-                    {' '}
+                    Having bachelors or masters in Mechanical / Electrical / Civil / Matlab /
+                    Mathematics / Chemistry / Physics / Accounts / Economics / Finance with grade
+                    more than 60%.{' '}
                   </li>
-                  <li>Having good rank in GATE/IES or NET-JRF/IIT- JAM</li>
-                  {' '}(Preferred rank under AIR 3000).
-                  <li>
-                    Willing to learn new concepts & skills over a period of time.
-                  </li>
+                  <li>Having good rank in GATE/IES or NET-JRF/IIT- JAM</li> (Preferred rank under
+                  AIR 3000).
+                  <li>Willing to learn new concepts & skills over a period of time.</li>
                   <li>Having Sound and diversified subject knowledge. </li>
                   <li>Proficient with Computer & use of Internet. </li>
                   <li>Willing to work in rotational shifts.</li>
                 </ul>
               </div>
-              <div className='industry'>
-                <h4>Industry: <span>E-Learning</span></h4>
+              <div className="industry">
+                <h4>
+                  Industry: <span>E-Learning</span>
+                </h4>
               </div>
               <div>
                 <h4>
@@ -61,15 +63,19 @@ export default class chat extends React.Component {
                 </h4>
               </div>
               <div>
-                <h4>Experience: <span>Fresher </span></h4>
+                <h4>
+                  Experience: <span>Fresher </span>
+                </h4>
               </div>
               <div>
-                <h4>Location: <span>Delhi</span></h4>
+                <h4>
+                  Location: <span>Delhi</span>
+                </h4>
               </div>
-              <div className='cta'>
+              <div className="cta">
                 <a
-                  className='btn'
-                  href='                      https://docs.google.com/a/tutorbro.com/forms/d/e/1FAIpQLSdxFNjOUrrZejqP5u_KSzvTSto5uWw-HACF3Y-hLqwnKvFZgg/viewform'
+                  className="btn"
+                  href="                      https://docs.google.com/a/tutorbro.com/forms/d/e/1FAIpQLSdxFNjOUrrZejqP5u_KSzvTSto5uWw-HACF3Y-hLqwnKvFZgg/viewform"
                 >
                   Apply Here
                 </a>
@@ -80,76 +86,76 @@ export default class chat extends React.Component {
         </main>
         <style jsx>
           {`
-        main {
-          padding-top: 10px;
-        }
-        .carrers__container {
-          max-width: 600px;
-          margin: 0 auto;
-        }
-        .carrers {
-          padding: 20px;
-          padding-bottom: 50px;
-        }
-        h1 {
-          text-align: center;
-          font-size: 3.1875rem;
-          margin: 0;
-        }
-        ul {
-          list-style-type: circle;
-          padding: 10px;
-          padding-left: 25px;
-          margin: 0;
-        }
-        h4 {
-          color: #4183C4;
-          margin-bottom: 0;
-          font-size: 1.3125rem;
-        }
-        h4 span {
-          font-size: 1.125rem;
-          color: #888;
-          letter-spacing: 2px;
-          line-height: 1.8;
-        }
-        p {
-          margin: 0;
-          margin-top: 10px;
-        }
-        .cta {
-          display: flex;
-          justify-content: center;
-          padding: 20px;
-        }
-        .btn {
-          -webkit-appearance: none;
-          cursor: pointer;
-          background: #2bc186;
-          border: none;
-          outline: none;
-          padding: 6px 24px;
-          border-radius: 17px;
-          color: #fff;
-          line-height: 24px;
-          font-weight: 600;
-          font-size: 14px;
-          transition: all 0.2s ease;
-          margin: 10px 0;
-          text-align: center;
-          text-decoration: none;
-        }
-        .btn:hover {
-          background: teal;
-        }
-        @media (max-width: 480px) {
-          h1 {
-            font-size: 32px;
-          }
-        }
-      `}
+            main {
+              padding-top: 10px;
+            }
+            .carrers__container {
+              max-width: 600px;
+              margin: 0 auto;
+            }
+            .carrers {
+              padding: 20px;
+              padding-bottom: 50px;
+            }
+            h1 {
+              text-align: center;
+              font-size: 3.1875rem;
+              margin: 0;
+            }
+            ul {
+              list-style-type: circle;
+              padding: 10px;
+              padding-left: 25px;
+              margin: 0;
+            }
+            h4 {
+              color: #4183c4;
+              margin-bottom: 0;
+              font-size: 1.3125rem;
+            }
+            h4 span {
+              font-size: 1.125rem;
+              color: #888;
+              letter-spacing: 2px;
+              line-height: 1.8;
+            }
+            p {
+              margin: 0;
+              margin-top: 10px;
+            }
+            .cta {
+              display: flex;
+              justify-content: center;
+              padding: 20px;
+            }
+            .btn {
+              -webkit-appearance: none;
+              cursor: pointer;
+              background: #2bc186;
+              border: none;
+              outline: none;
+              padding: 6px 24px;
+              border-radius: 17px;
+              color: #fff;
+              line-height: 24px;
+              font-weight: 600;
+              font-size: 14px;
+              transition: all 0.2s ease;
+              margin: 10px 0;
+              text-align: center;
+              text-decoration: none;
+            }
+            .btn:hover {
+              background: teal;
+            }
+            @media (max-width: 480px) {
+              h1 {
+                font-size: 32px;
+              }
+            }
+          `}
         </style>
       </div>
-    )
+    );
   }
 }
