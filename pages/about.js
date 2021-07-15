aff9cf7d6941e709500b448aeb2847141e86291f
thesumansaurav <dev@tutorbro.com>
2021-07-15 15:17:42 +0530

    about us updated

--- pages/about.js
+++ pages/about.js
@@ -5,29 +5,35 @@ import { initGA, logPageView } from '../utils/analytics';
 
 
 export default class chat extends React.Component {
-    componentDidMount() {
-        initGA();
-        logPageView();
-    }
-    render() {
-        return (
-            <div>
-                <Header title="TutorBro | About Us" />
-                <main>
-                    <div className="carrers__container">
-                        <section className="carrers">
+  componentDidMount() {
+    initGA();
+    logPageView();
+  }
+  render() {
+    return (
+      <div>
+        <Header title="TutorBro | About Us" />
+        <main>
+          <div className="carrers__container">
+            <section className="carrers">
 
-                            <h1>About Us</h1>
-                            <p>
+              <h1>About Us</h1>
+              <p>
 
-                                TutorBro aims to provide well-rounded online tutoring by being the hub of academic experts. Our mission is to help students improve their overall grades by providing quality tutoring at pocket-friendly prices.  We believe there are more teachers in the world than are actually teaching so we want to empower more people to learn from each other. Our platform enables thousands of tutors to share their knowledge with students around the globe. Whether you need help with high school algebra or you want to learn how to program in Python, we have a perfect tutor for you. At TutorBro, we want to change the way you learn.  We provide one-on-one live session with a subject expert as every student is unique and each one of them learns at a different pace. Our online experts are available 24/7 and have been making coursework comprehensive for every student.
-                            </p>
-                        </section>
-                    </div>
-                    <Footer />
-                </main>
-                <style jsx>
-                    {`
+                TutorBro aims to provide well-rounded online tutoring by being the hub of academic experts. Our mission is to help students improve their overall grades by providing quality tutoring at pocket-friendly prices.  We believe there are millions of teachers in the world that are teaching. We want to connect such teachers to the students across the globe in order to empower more people to learn from each other. Our platform enables thousands of tutors to share their knowledge with students across the globe. Whether you need help with high school algebra or you want to learn how to program in Python, we have a perfect tutor for you. At TutorBro, we want to change the way you learn.  We provide one-to-one live session with a subject expert as every student is unique and each one of them learns at a different pace. Our online experts are available 24/7 and have been making coursework comprehensive for every student.
+              </p>
+              <br></br><br></br>
+              <p>
+                Along the way, we have served thousands of students across the globe and they are doing well in their career. Till now we have more than 100000+ happy students from the 1000+ college and universities from different corner of the world. We have provided exact contents and assisted them throughout their academic years to achieve them what they are today. I can proudly say that TUTORBRO contributed extensively in making of career and academics of several students which are doing really great in their life.<br></br>
+                <b>Be a part of TUTORBRO and shine with confidence in your academic arena.</b>
+
+              </p>
+            </section>
+          </div>
+          <Footer />
+        </main>
+        <style jsx>
+          {`
             main {
               padding-top: 10px;
             }
@@ -104,8 +110,8 @@ export default class chat extends React.Component {
               }
             }
           `}
-                </style>
-            </div>
-        );
-    }
+        </style>
+      </div>
+    );
+  }
 }
