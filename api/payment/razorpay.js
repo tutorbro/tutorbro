fdae5c69ae949396e003a64c857ac21aabbfc188
ramnitesh <ramnitesh.saran@gmail.com>
2024-04-03 02:17:54 +0530

    redirect url changed for razor

--- api/payment/razorpay.js
+++ api/payment/razorpay.js
@@ -4,12 +4,12 @@ const razorpay = (request) => {
     key: "rzp_live_XzyAfLeHVMcrlS", // Enter the Key ID generated from the Dashboard
     amount: request.amount * 100, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
     currency: "INR",
-    name: "TutorBro Ltd", //your business name
+    name: "TutorBro Pvt Ltd", //your business name
     description: "TutorBro Transaction",
     image:
       "https://res.cloudinary.com/dtq6u9rp1/image/upload/v1711986571/tutorbro/tutorbro.png",
     //order_id: "order_9A33XWu170gUtm", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-    callback_url: "https://eneqd3r9zrjok.x.pipedream.net/",
+    callback_url: window.location.href,
     prefill: {
       //We recommend using the prefill parameter to auto-fill customer's contact information especially their phone number
       name: request.name, //your customer's name
