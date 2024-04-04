901251bb00aa7f6c48535e23c93a932caee35ade
ramnitesh <ramnitesh.saran@gmail.com>
2024-04-05 05:11:04 +0530

    meta changes

--- components/Meta.js
+++ components/Meta.js
@@ -18,12 +18,16 @@ export default ({ title }) => (
     /> */}
     <meta
       http-equiv="Content-Security-Policy"
-      content="default-src *; script-src-elem *;
+      content="default-src *; script-src-elem *; prefetch-src 'none';
 img-src 'self' data: https:; script-src 'self' 'unsafe-inline' 'unsafe-eval' *; 
 style-src  'self' 'unsafe-inline' *"
     ></meta>
     <title>{title}</title>
     <link rel="prefetch" href="https://tutorbro.com/payments"></link>
+    <link
+      rel="prefetch"
+      src="https://checkout.razorpay.com/v1/checkout.js"
+    ></link>
     <link
       rel="apple-touch-icon"
       sizes="57x57"
