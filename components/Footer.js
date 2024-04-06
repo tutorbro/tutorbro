39fd5e1a6e6d1c5fef16a96dc0c2536c4d030019
ramnitesh <ramnitesh.saran@gmail.com>
2024-04-06 15:00:52 +0530

    removed links

--- components/Footer.js
+++ components/Footer.js
@@ -28,12 +28,6 @@ export default class Footer extends React.Component {
             </div>
 
             <div className="links">
-              <Link href="/payments/payment-success" style={{ color: "white" }}>
-                <span className="link">Payment Status</span>
-              </Link>
-              <Link href="/payments" style={{ color: "white" }}>
-                <span className="link">Payments</span>
-              </Link>
               <Link href="/careers" style={{ color: "white" }}>
                 <span className="link">Careers</span>
               </Link>
