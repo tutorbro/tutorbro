b24fb6fef063389628bb92c074c7277ea7c2e3e3
ramnitesh <ramnitesh.saran@gmail.com>
2024-04-11 18:22:46 +0530

    removed package

--- api/payment/razorpay.js
+++ api/payment/razorpay.js
@@ -1,8 +1,6 @@
 import { RAZORPAY_KEY_ID, RAZORPAY_SECRET_KEY } from "../../config";
-import useRazorpay from "react-razorpay";
 
 function displayRazorpay(request) {
-  const [Razorpay] = useRazorpay();
   var options = {
     key: RAZORPAY_KEY_ID, // Enter the Key ID generated from the Dashboard
     key_secret: RAZORPAY_SECRET_KEY,
